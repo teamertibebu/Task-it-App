7bd868dace4730a4c7c7b21deb586079ac6584c5
Teamer Tibebu <teamertibebu@teamertibebu.local>
2020-09-07 14:19:31 -0500

    Creates completed tasks list and  untask & clear tasks buttons

--- handleSubmit.js
+++ handleSubmit.js
@@ -3,27 +3,42 @@ const App = {
 
   initialize: () => {
     $("#taskItButton").click(() => {
-      const task = $("#enterTask").val();
-      $("#enterTask").val("");
-      App.createTaskDiv(task);
+      if ($("#enterTask").val()) {
+        const task = $("#enterTask").val();
+        $("#enterTask").val("");
+        App.createTaskDiv(task);
+      }
     });
+
+    document
+      .getElementById("enterTask")
+      .addEventListener("keyup", function (event) {
+        if (event.code === "Enter") {
+          event.preventDefault();
+          if ($("#enterTask").val()) {
+            const task = $("#enterTask").val();
+            $("#enterTask").val("");
+            App.createTaskDiv(task);
+          }
+        }
+      });
   },
 
   createTaskDiv: (task) => {
     const html = `
     <div class='taskDiv' id='taskDiv${App.count}'>
-    <div class='task'>${task}</div>
+    <div class='task' contenteditable='true'>${task}</div>
     <button class='untaskItButton' id='untaskItButton${App.count}'>Untask it!</button>
-    <input type='checkbox' id='completedTask${App.count}'></input>
-    <label for='completedTask${App.count}'>Task Completed!</label>
+    <button class='completedTaskButton' id='completedTaskButton${App.count}'>Tasked It!</button>
     </div>`;
     const buttonId = `#untaskItButton${App.count}`;
-    const checkboxId = `completedTask${App.count}`;
+    const completedId = `#completedTaskButton${App.count}`;
+    const taskDivId = `#taskDiv${App.count}`;
 
     App.count++;
-    $("#lowerDiv").append(html);
+    $("#lowerDiv").prepend(html);
     App.untaskButton(buttonId);
-    App.completedTask(checkboxId, buttonId);
+    App.completedTask(completedId, taskDivId, buttonId);
   },
 
   untaskButton: (buttonId) => {
@@ -32,15 +47,46 @@ const App = {
     });
   },
 
-  completedTask: (checkboxId, buttonId) => {
-    const cb = document.getElementById(checkboxId);
-    $(cb).change(() => {
-      if (cb.checked) {
-        const temp = $(`#${checkboxId}`).parent("div");
-        $(`#${checkboxId}`).parent("div").remove();
-        $("#completedDiv").append(temp);
-      }
+  completedTask: (completedId, taskDivId, buttonId) => {
+    $(completedId).click(() => {
+      $(taskDivId).css({ "text-decoration": "line-through" });
+      const temp = $(taskDivId);
+      $(temp).remove();
+      $("#lowerDiv").append(temp);
+      $(buttonId).hide();
+      $(completedId).hide();
+      const check = $("<span class='check'>&#10003;</span>");
+      $(taskDivId).prepend(check);
+      App.clearCompletedTasks();
     });
+  },
+
+  clearCount: 0,
+
+  clickCount: 0,
+
+  clearCompletedTasks: () => {
+    if (App.clearCount === 0) {
+      const clearButton = $(
+        '<button id="clearButton"><span>Clear Completed Tasks</span></button>'
+      );
+      $("#footerDiv").append(clearButton);
+      App.clearCount++;
+    } else {
+      $(clearButton).show();
+    }
+    if (App.clickCount === 0) {
+      $("#clearButton").click(() => {
+        const lowerDivChildren = Array.from($("#lowerDiv").children());
+        lowerDivChildren.forEach((child) => {
+          if (child.style.cssText) {
+            $(child).remove();
+            $(clearButton).hide();
+          }
+        });
+      });
+      App.clickCount++;
+    }
   },
 };
 
